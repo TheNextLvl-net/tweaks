4791307c2b1db774adbc1b37e906a3635b93031e
NonSwag <mrminecraft00@gmail.com#>
2021-10-02 17:36:05 +0200

    update

--- src/main/java/net/nonswag/tnl/tweaks/Tweaks.java
+++ src/main/java/net/nonswag/tnl/tweaks/Tweaks.java
@@ -10,15 +10,40 @@ import net.nonswag.tnl.tweaks.listeners.TeleportListener;
 public class Tweaks extends TNLPlugin {
 
     @Override
-    public void onEnable() {
-        getCommandManager().registerCommands(new PingCommand(), new TPSCommand(), new DayCommand(),
-                new NightCommand(), new SunCommand(), new RainCommand(), new ThunderCommand(),
-                new OPCommand(), new DeOPCommand(), new FeedCommand(), new HealCommand(),
-                new FlyCommand(), new SpeedCommand(), new GamemodeCommand(), new InventoryCommand(),
-                new HeadCommand(), new EnderChestCommand(), new ItemCommand(), new EnchantCommand(),
-                new UnEnchantCommand(), new RepairCommand(), new ClearCommand(), new BackCommand());
+    public void enable() {
+        registerCommands();
+        registerListeners();
+        if (Settings.AUTO_UPDATER.getValue()) new PluginUpdate(this).downloadUpdate();
+    }
+
+    private void registerListeners() {
         getEventManager().registerListener(new TeleportListener());
         getEventManager().registerListener(new DeathListener());
-        if (Settings.AUTO_UPDATER.getValue()) new PluginUpdate(this).downloadUpdate();
+    }
+
+    private void registerCommands() {
+        getCommandManager().registerCommand(new PingCommand());
+        getCommandManager().registerCommand(new TPSCommand());
+        getCommandManager().registerCommand(new DayCommand());
+        getCommandManager().registerCommand(new NightCommand());
+        getCommandManager().registerCommand(new SunCommand());
+        getCommandManager().registerCommand(new RainCommand());
+        getCommandManager().registerCommand(new ThunderCommand());
+        getCommandManager().registerCommand(new OPCommand());
+        getCommandManager().registerCommand(new DeOPCommand());
+        getCommandManager().registerCommand(new FeedCommand());
+        getCommandManager().registerCommand(new HealCommand());
+        getCommandManager().registerCommand(new FlyCommand());
+        getCommandManager().registerCommand(new SpeedCommand());
+        getCommandManager().registerCommand(new GamemodeCommand());
+        getCommandManager().registerCommand(new InventoryCommand());
+        getCommandManager().registerCommand(new HeadCommand());
+        getCommandManager().registerCommand(new EnderChestCommand());
+        getCommandManager().registerCommand(new ItemCommand());
+        getCommandManager().registerCommand(new RepairCommand());
+        getCommandManager().registerCommand(new EnchantCommand());
+        getCommandManager().registerCommand(new UnEnchantCommand());
+        getCommandManager().registerCommand(new ClearCommand());
+        getCommandManager().registerCommand(new BackCommand());
     }
 }
