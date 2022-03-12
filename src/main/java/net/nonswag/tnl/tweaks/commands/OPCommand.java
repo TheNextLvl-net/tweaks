4638b0f364302285c85831c884727ec0e08ac8b3
NonSwag <mrminecraft00@gmail.com#>
2022-03-12 09:56:16 +0100

    updated "/op list" command

--- src/main/java/net/nonswag/tnl/tweaks/commands/OPCommand.java
+++ src/main/java/net/nonswag/tnl/tweaks/commands/OPCommand.java
@@ -3,6 +3,7 @@ package net.nonswag.tnl.tweaks.commands;
 import net.nonswag.tnl.core.api.command.CommandSource;
 import net.nonswag.tnl.core.api.command.Invocation;
 import net.nonswag.tnl.listener.api.command.TNLCommand;
+import net.nonswag.tnl.listener.api.command.exceptions.InvalidUseException;
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 
@@ -14,6 +15,7 @@ public class OPCommand extends TNLCommand {
 
     public OPCommand() {
         super("op", "tnl.rights");
+        setUsage("%prefix% §c/op §8[§6Player§8]");
     }
 
     @Override
@@ -29,21 +31,21 @@ public class OPCommand extends TNLCommand {
                         arg.setOp(true);
                         source.sendMessage("%prefix% §6" + arg.getName() + "§a is now an operator");
                     } else source.sendMessage("%prefix% §cNothing could be changed");
-                } else source.sendMessage("%prefix% §c/op §8[§6Player§8]");
+                } else throw new InvalidUseException(this);
             } else {
-                List<String> operators = new ArrayList<>();
-                for (OfflinePlayer player : Bukkit.getOperators()) operators.add(player.getName());
-                source.sendMessage("%prefix% §7Operators§8: §6" + String.join("§8, §6", operators));
+                List<String> s = new ArrayList<>();
+                for (OfflinePlayer player : Bukkit.getOperators()) s.add(player.getName());
+                if (s.isEmpty()) source.sendMessage("%prefix% §cThere are no operators");
+                else source.sendMessage("%prefix% §7Operators §8(§a" + s.size() + "§8): §6" + String.join("§8, §6", s));
             }
-        } else source.sendMessage("%prefix% §c/op §8[§6Player§8]");
+        } else throw new InvalidUseException(this);
     }
 
     @Nonnull
     @Override
     protected List<String> suggest(@Nonnull Invocation invocation) {
-        String[] args = invocation.arguments();
         List<String> suggestions = new ArrayList<>();
-        if (args.length > 1) return suggestions;
+        if (invocation.arguments().length > 1) return suggestions;
         for (OfflinePlayer all : Bukkit.getOfflinePlayers()) if (!all.isOp()) suggestions.add(all.getName());
         suggestions.add("list");
         return suggestions;
