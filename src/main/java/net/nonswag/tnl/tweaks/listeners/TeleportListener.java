7d14d03e34ce36c805b39c12c8ea08041233171f
NonSwag <mrminecraft00@gmail.com>
2022-08-23 00:22:00 +0200

    ignoring unknown teleport reason

--- src/main/java/net/nonswag/tnl/tweaks/listeners/TeleportListener.java
+++ src/main/java/net/nonswag/tnl/tweaks/listeners/TeleportListener.java
@@ -12,6 +12,7 @@ public class TeleportListener implements Listener {
 
     @EventHandler
     public void onTeleport(@Nonnull PlayerTeleportEvent event) {
+        if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.UNKNOWN)) return;
         TNLPlayer player = TNLPlayer.cast(event.getPlayer());
         BackCommand.setLastPosition(player, event.getFrom());
     }
