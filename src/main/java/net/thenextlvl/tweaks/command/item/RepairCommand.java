c206a57c1877eb7308c847eb7970b95862f43d15
NonSwag <mrminecraft00@gmail.com>
2023-04-15 14:49:28 +0200

    removed repair all permission

--- src/main/java/net/thenextlvl/tweaks/command/item/RepairCommand.java
+++ src/main/java/net/thenextlvl/tweaks/command/item/RepairCommand.java
@@ -2,8 +2,6 @@ package net.thenextlvl.tweaks.command.item;
 
 import net.thenextlvl.tweaks.command.api.CommandInfo;
 import net.thenextlvl.tweaks.command.api.CommandSenderException;
-import net.thenextlvl.tweaks.command.api.NoPermissionException;
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
@@ -11,8 +9,6 @@ import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.inventory.meta.Damageable;
-import org.bukkit.permissions.Permission;
-import org.bukkit.plugin.PluginManager;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.Collections;
@@ -26,14 +22,6 @@ import java.util.List;
 )
 public class RepairCommand implements TabExecutor {
 
-    private static final String REPAIR_ALL_PERMISSION = "tweaks.command.repair.all";
-
-    public RepairCommand() {
-        PluginManager pluginManager = Bukkit.getPluginManager();
-        if (pluginManager.getPermission(REPAIR_ALL_PERMISSION) == null)
-            pluginManager.addPermission(new Permission(REPAIR_ALL_PERMISSION));
-    }
-
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (!(sender instanceof Player player)) throw new CommandSenderException();
@@ -45,11 +33,7 @@ public class RepairCommand implements TabExecutor {
             return true;
         }
 
-        if (args.length == 1 && args[0].equalsIgnoreCase("hand"))
-            return onCommand(sender, command, label, new String[0]);
-
         if (args.length == 1 && args[0].equalsIgnoreCase("all")) {
-            if (!sender.hasPermission(REPAIR_ALL_PERMISSION)) throw new NoPermissionException(REPAIR_ALL_PERMISSION);
 
             @Nullable ItemStack[] contents = inventory.getContents();
             for (int i = 0; i < contents.length; i++) {
@@ -76,8 +60,7 @@ public class RepairCommand implements TabExecutor {
     }
 
     @Override
-    public @Nullable List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length < 1 || !sender.hasPermission(REPAIR_ALL_PERMISSION)) return Collections.emptyList();
-        return Collections.singletonList("all");
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return args.length <= 1 ? Collections.singletonList("all") : Collections.emptyList();
     }
 }
