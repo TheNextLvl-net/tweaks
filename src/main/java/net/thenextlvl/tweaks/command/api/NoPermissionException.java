550df0b2328d0754c877ed76a1d50c843dc68ca1
NonSwag <mrminecraft00@gmail.com>
2023-04-15 14:38:55 +0200

    optimized permission exception

---
+++ src/main/java/net/thenextlvl/tweaks/command/api/NoPermissionException.java
@@ -0,0 +1,19 @@
+package net.thenextlvl.tweaks.command.api;
+
+import core.api.placeholder.Placeholder;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.tweaks.util.Messages;
+import org.bukkit.command.CommandSender;
+
+import java.util.Locale;
+
+@RequiredArgsConstructor
+public class NoPermissionException extends CommandException {
+    private final String permission;
+
+    @Override
+    protected void handle(Locale locale, CommandSender sender) {
+        var placeholder = Placeholder.<CommandSender>of("permission", permission);
+        sender.sendPlainMessage(Messages.COMMAND_PERMISSION.message(locale, placeholder));
+    }
+}
