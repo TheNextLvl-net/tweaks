2f3d530250bc1414543400dd0f3bc2ebd3e25845
DevCyntrix <devcyntrix@hotmail.com>
2023-04-16 16:00:30 +0200

    Fix gamemode command

--- src/main/java/net/thenextlvl/tweaks/command/player/GameModeCommand.java
+++ src/main/java/net/thenextlvl/tweaks/command/player/GameModeCommand.java
@@ -2,6 +2,7 @@ package net.thenextlvl.tweaks.command.player;
 
 import net.thenextlvl.tweaks.command.api.CommandInfo;
 import net.thenextlvl.tweaks.command.api.CommandSenderException;
+import net.thenextlvl.tweaks.command.api.PlayerNotOnlineException;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.command.Command;
@@ -22,20 +23,22 @@ public class GameModeCommand implements TabExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length < 1)
-            return false;
+        if (args.length < 1) return false;
 
-        GameMode mode = GameMode.valueOf(args[0].toUpperCase());
-        if (mode == null) {
+        GameMode mode;
+        try {
+            mode = GameMode.valueOf(args[0].toUpperCase());
+        } catch (IllegalArgumentException e) {
             return false;
         }
+
         Player target;
-        if (args.length == 2)
-            target = Bukkit.getPlayer(args[1]);
-        else if (sender instanceof Player player)
-            target = player;
-        else
-            throw new CommandSenderException();
+        if (args.length == 2) target = Bukkit.getPlayer(args[1]);
+        else if (sender instanceof Player player) target = player;
+        else throw new CommandSenderException();
+
+        if (target == null) throw new PlayerNotOnlineException(args[1]);
+
 
         target.setGameMode(mode);
 
@@ -46,8 +49,7 @@ public class GameModeCommand implements TabExecutor {
     public @Nullable List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         if (args.length <= 1)
             return Arrays.stream(GameMode.values()).map(gameMode -> gameMode.name().toLowerCase()).toList();
-        else if (args.length == 2)
-            return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
+        else if (args.length == 2) return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
         return null;
     }
 }
