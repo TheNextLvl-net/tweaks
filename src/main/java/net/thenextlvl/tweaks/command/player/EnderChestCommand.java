435b468d995941ad15e90cc66096e7f7debff01e
NonSwag <mrminecraft00@gmail.com>
2023-07-09 02:51:48 +0200

    added edit permission

--- src/main/java/net/thenextlvl/tweaks/command/player/EnderChestCommand.java
+++ src/main/java/net/thenextlvl/tweaks/command/player/EnderChestCommand.java
@@ -1,10 +1,22 @@
 package net.thenextlvl.tweaks.command.player;
 
+import net.thenextlvl.tweaks.TweaksPlugin;
 import net.thenextlvl.tweaks.command.api.CommandInfo;
 import net.thenextlvl.tweaks.command.api.CommandSenderException;
+import net.thenextlvl.tweaks.command.api.PlayerNotAffectedException;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 @CommandInfo(
         name = "enderchest",
@@ -13,11 +25,18 @@ import org.jetbrains.annotations.Nullable;
         permission = "tweaks.command.enderchest",
         aliases = {"ec"}
 )
-public class EnderChestCommand extends PlayerCommand {
+public class EnderChestCommand extends PlayerCommand implements Listener {
+    private final Set<HumanEntity> viewers = Collections.newSetFromMap(new WeakHashMap<>());
+
+    public EnderChestCommand(TweaksPlugin plugin) {
+        Bukkit.getPluginManager().registerEvents(this, plugin);
+    }
 
     @Override
     protected void execute(CommandSender sender, Player target) {
         if (!(sender instanceof Player player)) throw new CommandSenderException();
+        if (!isAllowed(sender, target)) throw new PlayerNotAffectedException(target);
+        if (!player.equals(target)) viewers.add(player);
         player.openInventory(target.getEnderChest());
     }
 
