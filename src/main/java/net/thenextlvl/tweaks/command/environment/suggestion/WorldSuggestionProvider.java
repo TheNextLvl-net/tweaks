c20b9d9516a207724237ce025a007a9397a0624f
david <mrminecraft00@gmail.com>
2024-09-28 11:18:34 +0200

    Add WorldSuggestionProvider for command environment
    Introduce a new class, WorldSuggestionProvider, to supply world suggestions for commands. This class integrates with the TweaksPlugin and WorldCommand for dynamic suggestions based on active worlds. The provider filters and suggests world names containing the current input.

---
+++ src/main/java/net/thenextlvl/tweaks/command/environment/suggestion/WorldSuggestionProvider.java
@@ -0,0 +1,27 @@
+package net.thenextlvl.tweaks.command.environment.suggestion;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.tweaks.TweaksPlugin;
+import net.thenextlvl.tweaks.command.environment.WorldCommand;
+
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
+public class WorldSuggestionProvider<S> implements SuggestionProvider<S> {
+    private final TweaksPlugin plugin;
+    private final WorldCommand command;
+
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<S> context, SuggestionsBuilder builder) {
+        plugin.getServer().getWorlds().stream()
+                .filter(command::isWorldAffected)
+                .map(world -> world.key().asString())
+                .filter(name -> name.contains(builder.getRemaining()))
+                .forEach(builder::suggest);
+        return builder.buildFuture();
+    }
+}
