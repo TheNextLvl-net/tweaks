f342c660a0593e894136711f2806f7f994d6203e
david <mrminecraft00@gmail.com>
2024-09-28 10:31:53 +0200

    Remove command API package
    Deleted the entire command API package including classes such as CommandBuilder, CommandException, and associated exception classes. This cleanup likely targets unused or outdated code to streamline the codebase.

--- src/main/java/net/thenextlvl/tweaks/command/api/package-info.java
+++
@@ -1,6 +0,0 @@
-@ParametersAreNotNullByDefault
-@MethodsReturnNotNullByDefault
-package net.thenextlvl.tweaks.command.api;
-
-import core.annotation.MethodsReturnNotNullByDefault;
-import core.annotation.ParametersAreNotNullByDefault;
