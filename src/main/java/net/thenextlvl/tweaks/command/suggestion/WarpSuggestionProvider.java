a8ad0d078a2fc7a38fca43a52cc213b8ecf166b2
david <mrminecraft00@gmail.com>
2024-09-29 22:25:08 +0200

    Add WarpSuggestionProvider for warp name suggestions
    This commit introduces the WarpSuggestionProvider class to the project. The provider uses the brigadier library and integrates with the TweaksPlugin to offer warp name suggestions dynamically. It filters and suggests warp names based on the user's input in command contexts.

---
+++ src/main/java/net/thenextlvl/tweaks/command/suggestion/WarpSuggestionProvider.java
@@ -0,0 +1,31 @@
+package net.thenextlvl.tweaks.command.suggestion;
+
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.tweaks.TweaksPlugin;
+import net.thenextlvl.tweaks.model.NamedLocation;
+
+import java.util.concurrent.CompletableFuture;
+
+@RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
+public class WarpSuggestionProvider implements SuggestionProvider<CommandSourceStack> {
+    private final TweaksPlugin plugin;
+
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
+        return plugin.warpController().getWarps().thenApply(warps -> {
+            warps.stream()
+                    .map(NamedLocation::getName)
+                    .filter(name -> name.contains(builder.getRemaining()))
+                    .map(StringArgumentType::escapeIfRequired)
+                    .forEach(builder::suggest);
+            return builder.build();
+        });
+    }
+}
