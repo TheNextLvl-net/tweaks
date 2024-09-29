ac1c2625fa2fc67db1c6cc9d8bce6e42d0c479e8
david <mrminecraft00@gmail.com>
2024-09-29 22:26:41 +0200

    Rename 'config' package and add package-info to various packages
    Renamed the 'config' package to 'model' for better clarity. Additionally, added package-info.java files with @NotNull annotations to multiple packages for enforcing non-null constraints and better code documentation.

---
+++ src/main/java/net/thenextlvl/tweaks/gui/package-info.java
@@ -0,0 +1,8 @@
+@FieldsAreNotNullByDefault
+@MethodsReturnNotNullByDefault
+@ParametersAreNotNullByDefault
+package net.thenextlvl.tweaks.gui;
+
+import core.annotation.FieldsAreNotNullByDefault;
+import core.annotation.MethodsReturnNotNullByDefault;
+import core.annotation.ParametersAreNotNullByDefault;
