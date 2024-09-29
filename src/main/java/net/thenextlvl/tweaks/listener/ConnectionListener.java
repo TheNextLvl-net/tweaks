5662303506c2869452604aef49599b9a8ad7f114
david <mrminecraft00@gmail.com>
2024-09-29 22:23:12 +0200

    Add spawn location for new player event and cleanup on quit
    Implemented handling of `PlayerSpawnLocationEvent` to teleport players based on config settings. Added cleanup of ongoing player conversations and TPA requests on player quit to ensure proper resource management.

--- src/main/java/net/thenextlvl/tweaks/listener/ConnectionListener.java
+++ src/main/java/net/thenextlvl/tweaks/listener/ConnectionListener.java
@@ -8,26 +8,42 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.spigotmc.event.player.PlayerSpawnLocationEvent;
 
 @RequiredArgsConstructor
 public class ConnectionListener implements Listener {
     private final TweaksPlugin plugin;
 
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onPlayerSpawnLocation(PlayerSpawnLocationEvent event) {
+        var config = plugin.config().spawn();
+        if (config.location() == null) return;
+        if ((!config.teleportOnFirstJoin() || event.getPlayer().hasPlayedBefore())
+            && (!config.teleportOnJoin() || !event.getPlayer().hasPlayedBefore())) return;
+        event.setSpawnLocation(config.location());
+    }
+
     @EventHandler(priority = EventPriority.HIGH)
     public void onJoin(PlayerJoinEvent event) {
-        var permissionLevel = plugin.config().generalConfig().defaultPermissionLevel();
+        var permissionLevel = plugin.config().general().defaultPermissionLevel();
         if (permissionLevel != -1) event.getPlayer().sendOpLevel(permissionLevel);
-        if (!plugin.config().generalConfig().overrideJoinMessage()) return;
+        if (!plugin.config().general().overrideJoinMessage()) return;
         plugin.getServer().getOnlinePlayers().forEach(player -> plugin.bundle().sendMessage(player, "player.joined",
                 Placeholder.parsed("player", event.getPlayer().getName())));
         event.joinMessage(null);
     }
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onQuit(PlayerQuitEvent event) {
-        if (!plugin.config().generalConfig().overrideQuitMessage()) return;
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        if (!plugin.config().general().overrideQuitMessage()) return;
         plugin.getServer().getOnlinePlayers().forEach(player -> plugin.bundle().sendMessage(player, "player.left",
                 Placeholder.parsed("player", event.getPlayer().getName())));
         event.quitMessage(null);
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void cleanup(PlayerQuitEvent event) {
+        plugin.msgController().removeConversations(event.getPlayer());
+        plugin.tpaController().removeRequests(event.getPlayer());
     }
 }
