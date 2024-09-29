ed3093e1f296de1cd2e7f3917bb062c4bfc7e2f2
david <mrminecraft00@gmail.com>
2024-09-29 22:24:29 +0200

    Add WarpCommand for player teleportation to predefined warps
    Introduced a new WarpCommand class to enable players to teleport to predefined warp locations. The command checks for player permissions, uses a suggestion provider for available warps, and sends appropriate feedback messages based on success or failure. This enhances the teleportation functionalities within the TweaksPlugin.

---
+++ src/main/java/net/thenextlvl/tweaks/command/warp/WarpCommand.java
@@ -0,0 +1,44 @@
+package net.thenextlvl.tweaks.command.warp;
+
+import com.mojang.brigadier.Command;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.command.brigadier.Commands;
+import lombok.RequiredArgsConstructor;
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.thenextlvl.tweaks.TweaksPlugin;
+import net.thenextlvl.tweaks.command.suggestion.WarpSuggestionProvider;
+import org.bukkit.entity.Player;
+
+import static org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.COMMAND;
+
+@RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
+public class WarpCommand {
+    private final TweaksPlugin plugin;
+
+    public void register(Commands registrar) {
+        var command = Commands.literal(plugin.commands().warp().command())
+                .requires(stack -> stack.getSender() instanceof Player player
+                                   && player.hasPermission("extra-tweaks.warp"))
+                .then(Commands.argument("name", StringArgumentType.string())
+                        .suggests(new WarpSuggestionProvider(plugin))
+                        .executes(this::warp))
+                .build();
+        registrar.register(command, "Teleport to warps", plugin.commands().warp().aliases());
+    }
+
+    private int warp(CommandContext<CommandSourceStack> context) {
+        var player = (Player) context.getSource().getSender();
+        var name = context.getArgument("name", String.class);
+        plugin.warpController().getWarp(name).thenAccept(warp -> {
+            if (warp != null) plugin.teleportController().teleport(player, warp, COMMAND).thenAccept(success -> {
+                var message = success ? "command.warp" : "command.teleport.cancelled";
+                plugin.bundle().sendMessage(player, message, Placeholder.parsed("name", name));
+            });
+            else plugin.bundle().sendMessage(player, "command.warp.unknown", Placeholder.parsed("name", name));
+        });
+        return Command.SINGLE_SUCCESS;
+    }
+}
