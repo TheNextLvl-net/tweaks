c569f83b07ef488fd663b6f27d6abf5af1e1509d
david <mrminecraft00@gmail.com>
2024-09-29 22:23:35 +0200

    Add WarpGUI for handling player warp interactions
    This commit introduces the WarpGUI class, which extends NamedLocationGUI to facilitate warp interactions for players. It includes functionality to teleport players to named locations and sends appropriate success or failure messages based on the teleport outcome.

---
+++ src/main/java/net/thenextlvl/tweaks/gui/WarpGUI.java
@@ -0,0 +1,24 @@
+package net.thenextlvl.tweaks.gui;
+
+import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.thenextlvl.tweaks.TweaksPlugin;
+import net.thenextlvl.tweaks.model.NamedLocation;
+import org.bukkit.entity.Player;
+
+import java.util.Collection;
+
+import static org.bukkit.event.player.PlayerTeleportEvent.TeleportCause.PLUGIN;
+
+public class WarpGUI extends NamedLocationGUI {
+    public WarpGUI(TweaksPlugin plugin, Player owner, Collection<NamedLocation> elements) {
+        super(plugin, plugin.config().guis().warps(), owner, plugin.bundle().component(owner, "gui.title.warps"), elements);
+    }
+
+    @Override
+    protected void teleport(NamedLocation element) {
+        plugin.teleportController().teleport(owner, element, PLUGIN).thenAccept(success -> {
+            var message = success ? "command.warp" : "command.teleport.cancelled";
+            plugin.bundle().sendMessage(owner, message, Placeholder.parsed("name", element.getName()));
+        });
+    }
+}
