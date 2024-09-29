4e28bec7913149f18fe682897b86496e44ede4c4
david <mrminecraft00@gmail.com>
2024-09-29 22:25:31 +0200

    Add TwitchCommand to register Twitch social command
    Introduce `TwitchCommand` to enable a custom command that announces the plugin's Twitch channel. It registers the command with specific aliases and provides a message to users when executed.

---
+++ src/main/java/net/thenextlvl/tweaks/command/social/TwitchCommand.java
@@ -0,0 +1,20 @@
+package net.thenextlvl.tweaks.command.social;
+
+import com.mojang.brigadier.Command;
+import io.papermc.paper.command.brigadier.Commands;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.tweaks.TweaksPlugin;
+
+@RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
+public class TwitchCommand {
+    private final TweaksPlugin plugin;
+
+    public void register(Commands registrar) {
+        var command = Commands.literal(plugin.commands().twitch().command()).executes(context -> {
+            plugin.bundle().sendMessage(context.getSource().getSender(), "social.twitch");
+            return Command.SINGLE_SUCCESS;
+        }).build();
+        registrar.register(command, "Watch us on Twitch", plugin.commands().twitch().aliases());
+    }
+}
