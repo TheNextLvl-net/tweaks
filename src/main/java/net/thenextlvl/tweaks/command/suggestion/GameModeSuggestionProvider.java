b9b350d7348f67c7c5b4f938f4e975daa82e869e
david <mrminecraft00@gmail.com>
2024-09-29 21:28:02 +0200

    Add GameMode argument type and suggestions
    Introduce `GameModeArgumentType` to handle GameMode command arguments, mapping input strings to GameMode instances. Additionally, implement `GameModeSuggestionProvider` to offer auto-completion suggestions based on available GameMode values.

---
+++ src/main/java/net/thenextlvl/tweaks/command/suggestion/GameModeSuggestionProvider.java
@@ -0,0 +1,21 @@
+package net.thenextlvl.tweaks.command.suggestion;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import core.paper.command.SuggestionProvider;
+import org.bukkit.GameMode;
+
+import java.util.Arrays;
+import java.util.concurrent.CompletableFuture;
+
+public class GameModeSuggestionProvider implements SuggestionProvider {
+    @Override
+    public CompletableFuture<Suggestions> suggest(CommandContext<?> context, SuggestionsBuilder builder) {
+        Arrays.stream(GameMode.values())
+                .map(gameMode -> gameMode.name().toLowerCase())
+                .filter(name -> name.contains(builder.getRemaining()))
+                .forEach(builder::suggest);
+        return builder.buildFuture();
+    }
+}
