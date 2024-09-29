e54a09ec09926f68d93601f4eb7aa17fb8fdc12a
david <mrminecraft00@gmail.com>
2024-09-29 22:25:49 +0200

    Add YouTube command
    Implemented a new command for sharing the YouTube link through the Tweaks plugin. The command displays a message encouraging users to subscribe and supports aliases for easier access. This enhances social media engagement directly through the game.

---
+++ src/main/java/net/thenextlvl/tweaks/command/social/YouTubeCommand.java
@@ -0,0 +1,20 @@
+package net.thenextlvl.tweaks.command.social;
+
+import com.mojang.brigadier.Command;
+import io.papermc.paper.command.brigadier.Commands;
+import lombok.RequiredArgsConstructor;
+import net.thenextlvl.tweaks.TweaksPlugin;
+
+@RequiredArgsConstructor
+@SuppressWarnings("UnstableApiUsage")
+public class YouTubeCommand {
+    private final TweaksPlugin plugin;
+
+    public void register(Commands registrar) {
+        var command = Commands.literal(plugin.commands().youtube().command()).executes(context -> {
+            plugin.bundle().sendMessage(context.getSource().getSender(), "social.youtube");
+            return Command.SINGLE_SUCCESS;
+        }).build();
+        registrar.register(command, "Subscribe to us on YouTube", plugin.commands().youtube().aliases());
+    }
+}
