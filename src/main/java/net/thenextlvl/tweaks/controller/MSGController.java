b2045040eee9248b82bfe584257635fb95b0581b
david <mrminecraft00@gmail.com>
2024-09-29 22:23:49 +0200

    Add MSGController class for managing command sender conversations
    This commit introduces the MSGController class to handle conversations between command senders in a Minecraft plugin. The class provides methods to start, manage, and remove conversations using a WeakHashMap to track sender-receiver pairs. This enhances the plugin's functionality by facilitating message exchanges and conversation management.

---
+++ src/main/java/net/thenextlvl/tweaks/controller/MSGController.java
@@ -0,0 +1,38 @@
+package net.thenextlvl.tweaks.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Map;
+import java.util.WeakHashMap;
+
+@RequiredArgsConstructor
+public class MSGController {
+    private final Map<CommandSender, CommandSender> conversations = new WeakHashMap<>();
+
+    public @Nullable CommandSender getConversation(CommandSender sender) {
+        return conversations.get(sender);
+    }
+
+    public boolean isConversing(CommandSender sender, CommandSender receiver) {
+        return receiver.equals(conversations.get(sender));
+    }
+
+    public void startConversation(CommandSender sender, CommandSender receiver) {
+        conversations.put(sender, receiver);
+    }
+
+    public void removeConversation(CommandSender sender) {
+        conversations.remove(sender);
+    }
+
+    public void removeConversations(CommandSender sender) {
+        removeConversation(sender);
+        removeExternalConversations(sender);
+    }
+
+    public void removeExternalConversations(CommandSender sender) {
+        conversations.entrySet().removeIf(entry -> entry.getValue().equals(sender));
+    }
+}
