ab9f19921d8d802bcbade54a8e4531f1fe780ab0
david <mrminecraft00@gmail.com>
2024-09-29 11:22:30 +0200

    Refactor EnchantCommand to use Brigadier and add suggestions
    Refactored the EnchantCommand to leverage Brigadier for command parsing and execution. This includes adding an EnchantmentSuggestionProvider for dynamic suggestion of enchantments and integrating level suggestion functionality to streamline user experience.

---
+++ src/main/java/net/thenextlvl/tweaks/command/item/suggestion/EnchantmentSuggestionProvider.java
@@ -0,0 +1,28 @@
+package net.thenextlvl.tweaks.command.item.suggestion;
+
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.suggestion.SuggestionProvider;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import io.papermc.paper.command.brigadier.CommandSourceStack;
+import io.papermc.paper.registry.RegistryAccess;
+import io.papermc.paper.registry.RegistryKey;
+import org.bukkit.entity.Player;
+
+import java.util.concurrent.CompletableFuture;
+
+@SuppressWarnings("UnstableApiUsage")
+public class EnchantmentSuggestionProvider implements SuggestionProvider<CommandSourceStack> {
+    @Override
+    public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
+        if (!(context.getSource().getSender() instanceof Player player)) return builder.buildFuture();
+        var item = player.getInventory().getItemInMainHand();
+        RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT).stream()
+                .filter(enchantment -> enchantment.canEnchantItem(item))
+                .filter(enchantment -> item.getEnchantments().containsKey(enchantment)
+                                       || item.getEnchantments().keySet().stream().noneMatch(enchantment::conflictsWith))
+                .map(enchantment -> enchantment.key().asString())
+                .forEach(builder::suggest);
+        return builder.buildFuture();
+    }
+}
