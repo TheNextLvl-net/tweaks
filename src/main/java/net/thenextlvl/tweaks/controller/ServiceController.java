2571b3df09314e2ce6791a3caa14cc6d84743c11
david <mrminecraft00@gmail.com>
2024-09-30 20:00:26 +0200

    Add optional service controllers to ServiceController.
    This update introduces optional support for BankController, ChatController, EconomyController, and GroupController. Each service controller is loaded via a helper method that logs the usage of the service if available. This enhances flexibility in configuring the ServiceController.

--- src/main/java/net/thenextlvl/tweaks/controller/ServiceController.java
+++ src/main/java/net/thenextlvl/tweaks/controller/ServiceController.java
@@ -1,17 +1,38 @@
 package net.thenextlvl.tweaks.controller;
 
 import lombok.Getter;
+import net.thenextlvl.service.api.chat.ChatController;
+import net.thenextlvl.service.api.economy.EconomyController;
+import net.thenextlvl.service.api.economy.bank.BankController;
+import net.thenextlvl.service.api.group.GroupController;
 import net.thenextlvl.service.api.permission.PermissionController;
 import net.thenextlvl.tweaks.TweaksPlugin;
+import org.jetbrains.annotations.Nullable;
 
-import java.util.Objects;
+import java.util.function.Function;
 
 @Getter
 public class ServiceController {
-    private final PermissionController permissions;
+    private final @Nullable BankController banks;
+    private final @Nullable ChatController chat;
+    private final @Nullable EconomyController economy;
+    private final @Nullable GroupController groups;
+    private final @Nullable PermissionController permissions;
 
     public ServiceController(TweaksPlugin plugin) {
-        this.permissions = Objects.requireNonNull(plugin.getServer().getServicesManager().load(PermissionController.class));
-        plugin.getComponentLogger().info("Using ServiceIO ({}) as permission provider.", permissions.getName());
+        this.banks = loadController(plugin, BankController.class, BankController::getName, "bank");
+        this.chat = loadController(plugin, ChatController.class, ChatController::getName, "chat");
+        this.economy = loadController(plugin, EconomyController.class, EconomyController::getName, "economy");
+        this.groups = loadController(plugin, GroupController.class, GroupController::getName, "group");
+        this.permissions = loadController(plugin, PermissionController.class, PermissionController::getName, "permission");
+    }
+
+    private <C> @Nullable C loadController(TweaksPlugin plugin, Class<C> clazz, Function<C, String> name, String description) {
+        var controller = plugin.getServer().getServicesManager().load(clazz);
+        if (controller != null) plugin.getComponentLogger().info(
+                "Using {} as {} provider.",
+                name.apply(controller), description
+        );
+        return controller;
     }
 }
