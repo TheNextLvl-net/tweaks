0d68f17cce6235da9c1c5bb53ce29e457c9388e1
david <mrminecraft00@gmail.com>
2024-09-30 17:59:07 +0200

    Add multiple new commands definitions
    Added several new command definitions including day, midnight, night, noon, rain, sun, thunder, enchant, and others. This improves the configuration flexibility and allows for an extensive range of commands with associated synonyms.

--- src/main/java/net/thenextlvl/tweaks/model/CommandConfig.java
+++ src/main/java/net/thenextlvl/tweaks/model/CommandConfig.java
@@ -10,15 +10,49 @@ import java.util.Set;
 @Getter
 @Accessors(fluent = true, chain = false)
 public class CommandConfig {
+    private @SerializedName("day") CommandDefinition day = new CommandDefinition("day", Set.of());
+    private @SerializedName("midnight") CommandDefinition midnight = new CommandDefinition("midnight", Set.of());
+    private @SerializedName("night") CommandDefinition night = new CommandDefinition("night", Set.of());
+    private @SerializedName("noon") CommandDefinition noon = new CommandDefinition("noon", Set.of());
+
+    private @SerializedName("rain") CommandDefinition rain = new CommandDefinition("rain", Set.of());
+    private @SerializedName("sun") CommandDefinition sun = new CommandDefinition("sun", Set.of());
+    private @SerializedName("thunder") CommandDefinition thunder = new CommandDefinition("thunder", Set.of());
+
     private @SerializedName("delete-home") CommandDefinition deleteHome = new CommandDefinition("delete-home", Set.of("delhome"));
     private @SerializedName("home") CommandDefinition home = new CommandDefinition("home", Set.of());
     private @SerializedName("homes") CommandDefinition homes = new CommandDefinition("homes", Set.of());
     private @SerializedName("set-home") CommandDefinition setHome = new CommandDefinition("set-home", Set.of("sethome"));
 
+    private @SerializedName("enchant") CommandDefinition enchant = new CommandDefinition("enchant", Set.of());
+    private @SerializedName("head") CommandDefinition head = new CommandDefinition("head", Set.of("skull"));
+    private @SerializedName("lore") CommandDefinition lore = new CommandDefinition("lore", Set.of());
+    private @SerializedName("rename") CommandDefinition rename = new CommandDefinition("rename", Set.of());
+    private @SerializedName("repair") CommandDefinition repair = new CommandDefinition("repair", Set.of());
+    private @SerializedName("unbreakable") CommandDefinition unbreakable = new CommandDefinition("unbreakable", Set.of());
+    private @SerializedName("unenchant") CommandDefinition unenchant = new CommandDefinition("unenchant", Set.of());
+
     private @SerializedName("set-spawn") CommandDefinition setSpawn = new CommandDefinition("set-spawn", Set.of("setspawn"));
     private @SerializedName("spawn") CommandDefinition spawn = new CommandDefinition("spawn", Set.of());
 
+    private @SerializedName("back") CommandDefinition back = new CommandDefinition("back", Set.of());
+    private @SerializedName("enderchest") CommandDefinition enderchest = new CommandDefinition("enderchest", Set.of("ec"));
+    private @SerializedName("feed") CommandDefinition feed = new CommandDefinition("feed", Set.of());
+    private @SerializedName("fly") CommandDefinition fly = new CommandDefinition("fly", Set.of("flight"));
+    private @SerializedName("gamemode") CommandDefinition gamemode = new CommandDefinition("gamemode", Set.of("gm"));
+    private @SerializedName("god") CommandDefinition god = new CommandDefinition("god", Set.of("invincible"));
+    private @SerializedName("hat") CommandDefinition hat = new CommandDefinition("hat", Set.of());
+    private @SerializedName("heal") CommandDefinition heal = new CommandDefinition("heal", Set.of());
+    private @SerializedName("inventory") CommandDefinition inventory = new CommandDefinition("inventory", Set.of("invsee", "inv"));
+    private @SerializedName("offline-teleport") CommandDefinition offlineTeleport = new CommandDefinition("offline-teleport", Set.of("offline-tp", "otp", "tpo"));
+    private @SerializedName("ping") CommandDefinition ping = new CommandDefinition("ping", Set.of("latency", "ms"));
+    private @SerializedName("seen") CommandDefinition seen = new CommandDefinition("seen", Set.of("find"));
+    private @SerializedName("speed") CommandDefinition speed = new CommandDefinition("speed", Set.of());
+    private @SerializedName("vanish") CommandDefinition vanish = new CommandDefinition("vanish", Set.of("v", "invisible"));
+
+    private @SerializedName("broadcast") CommandDefinition broadcast = new CommandDefinition("broadcast", Set.of("bc"));
     private @SerializedName("lobby") CommandDefinition lobby = new CommandDefinition("lobby", Set.of("hub", "l"));
+    private @SerializedName("motd") CommandDefinition motd = new CommandDefinition("motd", Set.of());
 
     private @SerializedName("teleport-accept") CommandDefinition teleportAccept = new CommandDefinition("tpaccept", Set.of());
     private @SerializedName("teleport-ask") CommandDefinition teleportAsk = new CommandDefinition("tpa", Set.of("tpask"));
@@ -42,6 +76,15 @@ public class CommandConfig {
     private @SerializedName("website") CommandDefinition website = new CommandDefinition("website", Set.of());
     private @SerializedName("x") CommandDefinition x = new CommandDefinition("x", Set.of("twitter"));
     private @SerializedName("youtube") CommandDefinition youtube = new CommandDefinition("youtube", Set.of("yt"));
+
+    private @SerializedName("anvil") CommandDefinition anvil = new CommandDefinition("anvil", Set.of());
+    private @SerializedName("cartography-table") CommandDefinition cartographyTable = new CommandDefinition("cartography-table", Set.of("cartography"));
+    private @SerializedName("enchanting-table") CommandDefinition enchantingTable = new CommandDefinition("enchanting-table", Set.of("enchanting"));
+    private @SerializedName("grindstone") CommandDefinition grindstone = new CommandDefinition("grindstone", Set.of());
+    private @SerializedName("loom") CommandDefinition loom = new CommandDefinition("loom", Set.of());
+    private @SerializedName("smithing-table") CommandDefinition smithingTable = new CommandDefinition("smithing-table", Set.of("smithing"));
+    private @SerializedName("stonecutter") CommandDefinition stonecutter = new CommandDefinition("stonecutter", Set.of());
+    private @SerializedName("workbench") CommandDefinition workbench = new CommandDefinition("workbench", Set.of("wb"));
 
     @Getter
     @AllArgsConstructor
