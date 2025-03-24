445205815254b8e7264ce11737be3f3ab72ffd3a
david <github@nonswag.dev>
2025-03-24 17:04:30 +0100

    Upgrade Shadow plugin to version 9.0.0-beta9
    Updated the Shadow plugin from version 8.3.6 to 9.0.0-beta9 in the build script. This ensures compatibility with the latest features and improvements in the plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import net.minecrell.pluginyml.paper.PaperPluginDescription
 
 plugins {
     id("java")
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "9.0.0-beta9"
     id("xyz.jpenilla.run-paper") version "2.3.1"
     id("net.minecrell.plugin-yml.paper") version "0.6.0"
     id("io.papermc.hangar-publish-plugin") version "0.1.2"
