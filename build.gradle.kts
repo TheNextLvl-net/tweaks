febeb15168337c0782a07e9edb5ced4dfbb3568e
david <github@nonswag.dev>
2025-03-31 21:21:38 +0200

    [ci skip] Updated plugin-yml.paper to version 0.7.1
    Switched to `de.eldoria.plugin-yml.paper` and upgraded to 0.7.1.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
     id("java")
     id("com.gradleup.shadow") version "9.0.0-beta9"
     id("xyz.jpenilla.run-paper") version "2.3.1"
-    id("net.minecrell.plugin-yml.paper") version "0.6.0"
+    id("de.eldoria.plugin-yml.paper") version "0.7.1"
     id("io.papermc.hangar-publish-plugin") version "0.1.3"
     id("com.modrinth.minotaur") version "2.+"
 }
