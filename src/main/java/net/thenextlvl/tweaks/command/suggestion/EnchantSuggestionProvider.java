77c0b895efe552964e15426d24152176ebcb0391
david <github@nonswag.dev>
2025-06-22 10:26:47 +0200

    Added option to allow unsafe enchantments
    Added new config option `allow-unsafe-enchantments` to enable bypassing standard enchantment restrictions. Updated related command logic to respect this configuration.

--- src/main/java/net/thenextlvl/tweaks/command/suggestion/EnchantSuggestionProvider.java
+++ src/main/java/net/thenextlvl/tweaks/command/suggestion/EnchantSuggestionProvider.java
@@ -7,6 +7,7 @@ import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.registry.RegistryAccess;
 import io.papermc.paper.registry.RegistryKey;
+import net.thenextlvl.tweaks.TweaksPlugin;
 import org.bukkit.entity.Player;
 import org.jspecify.annotations.NullMarked;
 
@@ -14,13 +15,20 @@ import java.util.concurrent.CompletableFuture;
 
 @NullMarked
 public class EnchantSuggestionProvider implements SuggestionProvider<CommandSourceStack> {
+    private final TweaksPlugin plugin;
+
+    public EnchantSuggestionProvider(TweaksPlugin plugin) {
+        this.plugin = plugin;
+    }
+
     @Override
     public CompletableFuture<Suggestions> getSuggestions(CommandContext<CommandSourceStack> context, SuggestionsBuilder builder) {
         if (!(context.getSource().getSender() instanceof Player player)) return builder.buildFuture();
         var item = player.getInventory().getItemInMainHand();
         RegistryAccess.registryAccess().getRegistry(RegistryKey.ENCHANTMENT).stream()
-                .filter(enchantment -> enchantment.canEnchantItem(item))
-                .filter(enchantment -> item.getEnchantments().containsKey(enchantment)
+                .filter(enchantment -> plugin.config().general.unsafeEnchantments || enchantment.canEnchantItem(item))
+                .filter(enchantment -> plugin.config().general.unsafeEnchantments
+                                       || item.getEnchantments().containsKey(enchantment)
                                        || item.getEnchantments().keySet().stream().noneMatch(enchantment::conflictsWith))
                 .map(enchantment -> enchantment.key().asString())
                 .filter(s -> s.contains(builder.getRemaining()))
