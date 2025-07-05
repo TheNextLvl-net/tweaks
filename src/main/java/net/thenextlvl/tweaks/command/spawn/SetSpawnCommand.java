5c7ebf40f406bc1b122ce9902dbc041630c047ad
david <github@nonswag.dev>
2025-07-05 12:21:49 +0200

    Refactored `SetSpawnCommand` to use `Rotation` object
    Replaced yaw/pitch arguments with `Rotation` for cleaner and more extensible code. Simplified position and rotation handling in spawn logic.

--- src/main/java/net/thenextlvl/tweaks/command/spawn/SetSpawnCommand.java
+++ src/main/java/net/thenextlvl/tweaks/command/spawn/SetSpawnCommand.java
@@ -1,14 +1,15 @@
 package net.thenextlvl.tweaks.command.spawn;
 
 import com.mojang.brigadier.Command;
-import com.mojang.brigadier.arguments.FloatArgumentType;
 import com.mojang.brigadier.builder.RequiredArgumentBuilder;
 import com.mojang.brigadier.context.CommandContext;
 import io.papermc.paper.command.brigadier.CommandSourceStack;
 import io.papermc.paper.command.brigadier.Commands;
 import io.papermc.paper.command.brigadier.argument.ArgumentTypes;
 import io.papermc.paper.command.brigadier.argument.resolvers.FinePositionResolver;
+import io.papermc.paper.command.brigadier.argument.resolvers.RotationResolver;
 import io.papermc.paper.math.FinePosition;
+import io.papermc.paper.math.Rotation;
 import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.thenextlvl.tweaks.TweaksPlugin;
@@ -31,7 +32,7 @@ public class SetSpawnCommand {
                 .requires(stack -> stack.getSender().hasPermission("tweaks.command.setspawn"))
                 .then(position()).executes(context -> {
                     var location = context.getSource().getLocation();
-                    return setSpawn(context, location.getWorld(), location, location.getYaw(), location.getPitch());
+                    return setSpawn(context, location.getWorld(), location, Rotation.rotation(location.getYaw(), location.getPitch()));
                 }).build();
         registrar.register(command, "Set the spawn location", plugin.commands().setSpawn.aliases);
     }
@@ -43,38 +44,34 @@ public class SetSpawnCommand {
                     var resolver = context.getArgument("position", FinePositionResolver.class);
                     var position = resolver.resolve(context.getSource());
                     var world = context.getSource().getLocation().getWorld();
-                    return setSpawn(context, world, position, 0, 0);
+                    return setSpawn(context, world, position, Rotation.rotation(0, 0));
                 });
     }
 
-    private RequiredArgumentBuilder<CommandSourceStack, Float> rotation() {
-        return Commands.argument("yaw", FloatArgumentType.floatArg(-180, 180))
-                .then(Commands.argument("pitch", FloatArgumentType.floatArg(-180, 180))
-                        .then(world())
-                        .executes(context -> {
-                            var yaw = context.getArgument("yaw", float.class);
-                            var pitch = context.getArgument("pitch", float.class);
-                            var resolver = context.getArgument("position", FinePositionResolver.class);
-                            var position = resolver.resolve(context.getSource());
-                            var world = context.getSource().getLocation().getWorld();
-                            return setSpawn(context, world, position, yaw, pitch);
-                        }));
+    private RequiredArgumentBuilder<CommandSourceStack, RotationResolver> rotation() {
+        return Commands.argument("rotation", ArgumentTypes.rotation()).executes(context -> {
+            var rotation = context.getArgument("rotation", RotationResolver.class);
+            var resolver = context.getArgument("position", FinePositionResolver.class);
+            var position = resolver.resolve(context.getSource());
+            var world = context.getSource().getLocation().getWorld();
+            return setSpawn(context, world, position, rotation.resolve(context.getSource()));
+        }).then(world());
     }
 
     private RequiredArgumentBuilder<CommandSourceStack, World> world() {
         return Commands.argument("world", ArgumentTypes.world())
                 .executes(context -> {
-                    var yaw = context.getArgument("yaw", float.class);
-                    var pitch = context.getArgument("pitch", float.class);
+                    var rotation = context.getArgument("rotation", RotationResolver.class);
                     var resolver = context.getArgument("position", FinePositionResolver.class);
                     var position = resolver.resolve(context.getSource());
                     var world = context.getArgument("world", World.class);
-                    return setSpawn(context, world, position, yaw, pitch);
+                    return setSpawn(context, world, position, rotation.resolve(context.getSource()));
                 });
     }
 
-    private int setSpawn(CommandContext<CommandSourceStack> context, World world, FinePosition position, float yaw, float pitch) {
-        plugin.config().spawn.location = position.toLocation(world);
+    private int setSpawn(CommandContext<CommandSourceStack> context, World world, FinePosition position, Rotation rotation) {
+        var spawn = position.toLocation(world).setRotation(rotation);
+        plugin.config().spawn.location = spawn;
         plugin.saveConfig();
 
         if (Boolean.TRUE.equals(world.getGameRuleValue(GameRule.SEND_COMMAND_FEEDBACK)))
@@ -83,11 +80,11 @@ public class SetSpawnCommand {
                     Formatter.number("x", position.x()),
                     Formatter.number("y", position.y()),
                     Formatter.number("z", position.z()),
-                    Formatter.number("yaw", yaw),
-                    Formatter.number("pitch", pitch));
+                    Formatter.number("yaw", rotation.yaw()),
+                    Formatter.number("pitch", rotation.pitch()));
 
         if (context.getSource().getSender() instanceof Player player)
-            player.teleportAsync(position.toLocation(world), PlayerTeleportEvent.TeleportCause.COMMAND);
+            player.teleportAsync(spawn, PlayerTeleportEvent.TeleportCause.COMMAND);
 
         return Command.SINGLE_SUCCESS;
     }
